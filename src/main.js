e48f1160d6268af3c28f210309d5030172ab130c
Behzod <behzodtuit@gmail.com>
2022-09-26 08:54:28 +0500

    behzod

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 import store from './store'
 import i18n from './i18n'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/en/api/v1/'
+axios.defaults.baseURL = 'https://grandhospital.pythonanywhere.com/en/api/v1/'
 createApp(App).use(i18n).use(store).use(router).mount('#app')
