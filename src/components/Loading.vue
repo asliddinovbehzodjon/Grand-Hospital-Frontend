2e3f1c0747b972315d77d36c04170fa410eafab8
Behzod <behzodtuit@gmail.com>
2022-09-28 20:20:09 +0500

    behzod

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -16,22 +16,5 @@
     </script>
     
     <style scoped>
-    .loading {
-      position: relative;
-
-      z-index: 1000;
-      height: 100%;
-      width: 100%;
-      text-align: center;
-      line-height: 100%;
-      transition: ease;
-    }
-    .loading img {
-      
-      position: absolute;
-      width: 20%;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%,50%);
-    }
+    @import '../assets/css/loading.css';
     </style>
